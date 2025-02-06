283425ab42c0f5626041424415c8f8f4f2ca4004
Kiana Bakrani Balani <kianabakrani@gmail.com>
2025-02-05 21:29:29 -0800

    Adding the final comment

--- src/main/java/helloworld/App.java
+++ src/main/java/helloworld/App.java
@@ -1,13 +1,13 @@
 package helloworld;
 
 /**
- * Hello world!
+ * Hello world Program using maven!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!!" );
     }
 }
